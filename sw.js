72126a166db8079532b2ba605e9c98d7103ecafe
zengjialong <zengjialong@bytedance.com>
2023-11-08 11:46:17 +0800

    chore: release

--- sw.js
+++ sw.js
@@ -27,10 +27,10 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f048fcd84ac85dfdebd1.js"
+    "url": "webpack-runtime-4fa3be823fbdcca4aa68.js"
   },
   {
-    "url": "styles.e43e6d0eaf695f6df485.css"
+    "url": "styles.c1e35e9a3db9b5aeb978.css"
   },
   {
     "url": "styles-407fe62976dc5310c43e.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "17cf33f815e045c46890f4130672664c"
+    "revision": "7ff01f68b855f2eef05cc39fe4f85a2c"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
